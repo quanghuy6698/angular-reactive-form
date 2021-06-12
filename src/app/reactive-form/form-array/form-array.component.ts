87d51b4bf257c10501d25483c15d7046b9541119
unknown <quanghuy6698@gmail.com>
2021-06-13 00:45:58 +0700

    [13/6/2021] form array

--- src/app/reactive-form/form-array/form-array.component.ts
+++ src/app/reactive-form/form-array/form-array.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: "form-array",
-  templateUrl: "./form-array.component.html",
-  styleUrls: ["./form-array.component.css"],
+  selector: 'form-array',
+  templateUrl: './form-array.component.html',
+  styleUrls: ['./form-array.component.css'],
 })
-export class FormArrayComponent {}
+export class FormArrayComponent {
+  demoForm = this.formBuilder.group({
+    school: [],
+    classes: this.formBuilder.array([]),
+  });
+  constructor(private formBuilder: FormBuilder) {}
+}
