be8aaf9c2965f3b9bd4d910868a6c8ee4bbb6386
quanghuy6698 <quanghuy6698@gmail.com>
2021-06-16 20:41:59 +0700

    [16/6/2021] form array update

--- src/app/reactive-form/form-array/form-array.component.ts
+++ src/app/reactive-form/form-array/form-array.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IProduct } from 'src/app/shared/constants/product.model';
 
 @Component({
   selector: 'form-array',
@@ -6,146 +8,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-array.component.css'],
 })
 export class FormArrayComponent implements OnInit {
-  createForm = this.formBuilder.group({
-    channel: [1],
-    careScriptName: [],
-    careScriptSteps: this.formBuilder.array([]),
+  productGroupForm = this.formBuilder.group({
+    productGroup: [''],
+    productList: this.formBuilder.array([]),
   });
 
-  isSaving = false;
-  actions: ICareScriptAction[];
-  channels = CHANNELS;
-  currentChannel = 1;
+  productListDisplay: IProduct[] = [];
 
-  constructor(
-    private formBuilder: FormBuilder,
-    protected activatedRoute: ActivatedRoute,
-    private careScriptService: CareScriptService,
-    private careScriptActionService: CareScriptActionService,
-    public dialog: MatDialog
-  ) {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.careScriptActionService.query({ channelId: 1 }).subscribe(res => {
-      this.actions = res.body || [];
-      this.newStep(0);
-    });
+    this.newProduct(0);
   }
 
-  get careScriptSteps() {
-    return this.createForm.controls['careScriptSteps'] as FormArray;
+  /**
+   * Get productList
+   *
+   * @return productList
+   */
+  get productList() {
+    return this.productGroupForm.controls['productList'] as FormArray;
   }
 
-  getFormGroupAtIndex(index: number) {
-    return this.careScriptSteps.at(index) as FormGroup;
+  /**
+   * Get productForm at index in productList
+   *
+   * @param index refer productForm index in productList
+   * @returns productForm
+   */
+  getProductForm(index: number) {
+    return this.productList.at(index) as FormGroup;
   }
 
-  createFromForm(): any {
-    return {
-      careScriptName: this.createForm.get(['careScriptName'])!.value,
-      channelId: this.createForm.get(['channel'])!.value,
-      careScriptSteps: this.convertStepValues(),
-    };
-  }
-
-  save() {
-    this.isSaving = true;
-    const param = this.createFromForm();
-
-    this.careScriptService.saveScriptStep({ ...param }).subscribe(
-      res => {
-        this.isSaving = false;
-        this.previousState();
-      },
-      error => {
-        this.isSaving = false;
-      }
-    );
-  }
-
-  createStepForm(): FormGroup {
-    return this.formBuilder.group({
-      action: [this.actions[0].id],
-      delay: [5, [Validators.required, Validators.min(1)]],
-      limitPerDay: [0, [Validators.required, Validators.min(0)]],
-    });
-  }
-
-  newStep(afterIndex: number) {
-    this.careScriptSteps.insert(afterIndex + 1, this.createStepForm());
-  }
-
-  removeStep(index: number) {
-    if (this.careScriptSteps.length <= 1) {
-      return;
-    } else {
-      this.careScriptSteps.removeAt(index);
-    }
-  }
-
-  // reselectStepAction() {
-  //   for (let i = 0; i < this.careScriptSteps.length; i++) {
-  //     const step = this.getFormGroupAtIndex(i);
-  //     const stepActionId = step.get('action').value;
-  //     step.controls['action'].setValue(stepActionId);
-  //   }
-  // }
-
-  convertStepValues() {
-    const doStepValues = this.careScriptSteps.value;
-    const stepValues = [];
-    let seq = 0;
-    doStepValues.forEach(value => {
-      stepValues.push({
-        sequence: seq++,
-        delay: Number(value.delay),
-        careScriptAction: {
-          id: value.action,
-        },
-        limitPerDay: value.limitPerDay,
+  /**
+   * Simple display product list
+   */
+  createProductGroup() {
+    const doProductListDisplay: IProduct[] = [];
+    const doProductListValue = this.productList.value;
+    doProductListValue.forEach((product: any) => {
+      doProductListDisplay.push({
+        name: product.name,
+        quantity: product.quantity,
+        price: product.price,
       });
     });
-    return stepValues;
+    this.productListDisplay = doProductListDisplay;
   }
 
-  // selectChannel(event: any) {
-  //   const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-  //     maxWidth: '400px',
-  //     data: reloadChannelActionConfirmDialogConfig,
-  //   });
-
-  //   dialogRef.afterClosed().subscribe(dialogResult => {
-  //     if (dialogResult) {
-  //       const channelId = event.target.value;
-  //       this.currentChannel = channelId;
-  //       this.careScriptActionService.query({ channelId: channelId }).subscribe(res => {
-  //         this.actions = res.body || [];
-  //         this.reselectStepAction();
-  //       });
-  //     } else {
-  //       this.createForm.controls['channel'].setValue(this.currentChannel);
-  //     }
-  //   });
-  // }
-
-  selectChannel(event: any) {
-    const channelId = event.target.value;
-    this.careScriptActionService.query({ channelId: channelId }).subscribe(res => {
-      this.actions = res.body || [];
-      this.clearAllSteps();
+  /**
+   * Create a product form
+   *
+   * @returns product form
+   */
+  createProductForm(): FormGroup {
+    return this.formBuilder.group({
+      name: [''],
+      quantity: [0, [Validators.required, Validators.min(0)]],
+      price: [1, [Validators.required, Validators.min(1)]],
     });
   }
 
-  clearAllSteps() {
-    this.careScriptSteps.clear();
-    this.careScriptSteps.push(this.createStepForm());
+  /**
+   * Add new product after index
+   *
+   * @param afterIndex refer to productForm index stand in front of productForm will add
+   */
+  newProduct(afterIndex: number) {
+    this.productList.insert(afterIndex + 1, this.createProductForm());
   }
 
-  validSpaceName(event: any) {
-    this.createForm.controls['careScriptName'].setValue(validSpace(event.target.value, 'careScriptName', this.createForm));
-  }
-
-  previousState() {
-    window.history.back();
+  /**
+   * Delete product at index
+   *
+   * @param index refer to productForm index in productList
+   */
+  deleteProduct(index: number) {
+    if (this.productList.length <= 1) {
+      return;
+    } else {
+      this.productList.removeAt(index);
+    }
   }
 }
